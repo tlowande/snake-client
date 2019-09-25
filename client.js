380afdc751bab2d1ad58faf157cdbcbb6e3cd6a4
Tamires <tamires.lowande@gmail.com>
2019-09-25 16:56:26 +0000

    client refactored and play call

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net')
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.2.204',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+
+
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-}
-
+const connect = require('./client')
 console.log('Connecting ...');
 connect();

fbdc2d9d85ec5e4539e3dc57eea5ec02cac6682d
Tamires <tamires.lowande@gmail.com>
2019-09-25 18:02:38 +0000

    refqactoring play

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  const handleUserInput = () => {
+    stdin.on("data", (input) => {
+      if (input === '\u0003') {
+        process.exit()
+      }
+    })
+  } 
+  handleUserInput()
+  stdin.resume();
+  
+  return stdin;
+}
+
+
+module.exports = setupInput
--- play.js
+++ play.js
@@ -1,23 +1,10 @@
 const connect = require('./client')
-console.log('Connecting ...');
-connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  const handleUserInput = () => {
-    stdin.on("data", (input) => {
-      if (input === '\u0003') {
-        process.exit()
-      }
-    })
-  } 
-  handleUserInput()
-  stdin.resume();
-  
-  return stdin;
-}
+const setupInput = require('./input');
+
+console.log('Connecting ...');
+
+connect();
 
 setupInput()
 
